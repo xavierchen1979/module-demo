45a60369a634536fbdf37a6901cfdf8910d5e511
Xavier <chenchopan@gmail.com>
2018-03-28 15:06:42 +0800

    Initial commit
    first commit

---
+++ index.py
@@ -0,0 +1,11 @@
+import datetime
+print(dir(datetime))
+print(datetime.__file__)
+print(datetime.datetime.now())
+print(datetime.date.today())
+
+
+from datetime import datetime 
+from datetime import date as mydate
+print(datetime.now())
+print(mydate.today())
---
+++ subpg2/reltest.py
@@ -0,0 +1,2 @@
+def test():
+    print('reltest sub2')
